190b2d4c64d1885074015c25bb1fcd432ff662d9
Sadaf Rasheed <sadaf@discretelogix.com>
2016-08-31 17:52:58 +0500

    Fixed Comp Requests URLs on dashboard

--- db/migrate/20160822194260_change_columns.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumns < ActiveRecord::Migration
-  def change
-    add_foreign_key :requests, :users, column: :initiator_id
-    add_foreign_key :requests, :users, column: :receiver_id
-    add_foreign_key :requests, :tenant_records, column: :comp_id
-
-  end
-end
