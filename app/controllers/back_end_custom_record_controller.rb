77cf35094838a295350565d5156f2c918687f8af
jahanzaib-dx <muhammad.jahanzaib@discretelogix.com>
2016-11-08 14:59:25 +0500

    multiple custom records

--- app/controllers/back_end_custom_record_controller.rb
+++ app/controllers/back_end_custom_record_controller.rb
@@ -3,13 +3,14 @@ class BackEndCustomRecordController < ApplicationController
   require 'digest/sha1'
   require 'time'
   def index
-    @custom_record_properties = CustomRecordProperty.select(:key).distinct.where('custom_record_id=?','1')
+    @custom_record_id = params[:id];
+    @custom_record_properties = CustomRecordProperty.select(:key).distinct.where('custom_record_id=?',@custom_record_id)
 
     time = Time.now.getutc
     fileName = Digest::SHA1.hexdigest("#{time}#{@current_user}")
     session = GoogleDrive::Session.from_config("#{Rails.root}/config/google-sheets.json")
 
-    check = BackEndCustomRecord.where('custom_record_id = ?', '1')
+    check = BackEndCustomRecord.where('custom_record_id = ?', @custom_record_id)
     if  check.count == 0
       @file = session.drive.copy_file('1Ey15FKMBqr8oNsGRXnRrSvUxPoWTgJwLmi3wkSV76Tk', {name: fileName}, {})
 
@@ -20,7 +21,7 @@ class BackEndCustomRecordController < ApplicationController
         row=1
         ws[row,col] = crp.key
         row+=1
-        @crp_values = CustomRecordProperty.select(:value).where('key = ? and custom_record_id = ?', crp.key,'1')
+        @crp_values = CustomRecordProperty.select(:value).where('key = ? and custom_record_id = ?', crp.key,@custom_record_id)
         @crp_values.each do |crp_value|
           ws[row, col] = crp_value.value
           row+=1
@@ -30,7 +31,7 @@ class BackEndCustomRecordController < ApplicationController
       ws.save()
 
       @BackEndCustonRecord = BackEndCustomRecord.new
-      @BackEndCustonRecord.custom_record_id = 1
+      @BackEndCustonRecord.custom_record_id = @custom_record_id
       @BackEndCustonRecord.file = @file.id
       @BackEndCustonRecord.save
 
@@ -44,16 +45,16 @@ class BackEndCustomRecordController < ApplicationController
         }
         session.drive.create_permission(@file_temp.id, user_permission, fields: 'id')
       end
-      @file = BackEndCustomRecord.where('custom_record_id = ?', '1').first
+      @file = BackEndCustomRecord.where('custom_record_id = ?', @custom_record_id).first
     else
-      @file = BackEndCustomRecord.where('custom_record_id = ?', '1').first
+      @file = BackEndCustomRecord.where('custom_record_id = ?', @custom_record_id).first
       ws = session.spreadsheet_by_key(@file.file).worksheets[0]
       col=1
       @custom_record_properties.each do |crp|
         row=1
         ws[row,col] = crp.key
         row+=1
-        @crp_values = CustomRecordProperty.select(:value).where('key = ? and custom_record_id = ?', crp.key,'1')
+        @crp_values = CustomRecordProperty.select(:value).where('key = ? and custom_record_id = ?', crp.key,@custom_record_id)
         @crp_values.each do |crp_value|
           ws[row, col] = crp_value.value
           row+=1
@@ -74,23 +75,24 @@ class BackEndCustomRecordController < ApplicationController
     end
     render :json => {
         :file_temp => @file_temp.id,
-        :file => @file.file
+        :file => @file.file,
+        :custom_record_id => @custom_record_id
     }
   end
 
 
   def create
-
+    @custom_record_id = params[:custom_record_id];
     session = GoogleDrive::Session.from_config("#{Rails.root}/config/google-sheets.json")
     session.drive.delete_file(params[:id])
     @file = session.drive.copy_file("#{params[:temp]}", {name: params[:id]}, {})
 
-    @BackEndCustomRecord = BackEndCustomRecord.where("custom_record_id = ?",'1').first
+    @BackEndCustomRecord = BackEndCustomRecord.where("custom_record_id = ?",@custom_record_id).first
     @BackEndCustomRecord.update_attributes(:file => @file.id)
 
     session.drive.delete_file(params[:temp])
     ws = session.spreadsheet_by_key(@file.id).worksheets[0]
-    @custom_record_properties = CustomRecordProperty.where("custom_record_id = ?",'1')
+    @custom_record_properties = CustomRecordProperty.where("custom_record_id = ?",@custom_record_id)
     @custom_record_properties.destroy_all
     key=''
     (1..ws.num_cols).each do |col|
@@ -101,7 +103,7 @@ class BackEndCustomRecordController < ApplicationController
           next
         else
           @custom_record_properties = CustomRecordProperty.new
-          @custom_record_properties.custom_record_id = 1
+          @custom_record_properties.custom_record_id = @custom_record_id
           @custom_record_properties.key = key
           @custom_record_properties.value = ws[row,col]
           @custom_record_properties.save
