6a3597f58c69ecbe39e4b66f575116ceda95693a
aamir-sarwar <aamir.sarwar@discretelogix.com>
2016-11-22 16:38:16 +0500

    Comprequest Updated

--- app/controllers/comp_requests_controller.rb
+++ app/controllers/comp_requests_controller.rb
@@ -5,20 +5,23 @@ class CompRequestsController < ApplicationController
 
   def index
     direction = params[:direction]
+    comp_type = params[:comp_type]
+
     if direction == 'incoming'
-      comp_type = params[:comp_type]
+
       if !comp_type.blank?
-        @comp_requests=CompRequest.incoming_sale_lease(current_user.id,comp_type)
-        @comp_requests.each do |comp_request|
-          comp = CompRequest.find(comp_request)
-          comp.status='TRUE'
-          comp.save
-        end
+
+         @comp_requests=CompRequest.incoming_sale_lease(current_user.id,comp_type)
+        # @comp_requests.each do |comp_request|
+        #   comp = CompRequest.find(comp_request)
+        #   comp.status='TRUE'
+        #   comp.save
+        # end
       else
-        @comp_requests = CompRequest.received_by current_user.id
+        @comp_requests = CompRequest.received_by current_user.id,comp_type
       end
     else
-      @comp_requests = CompRequest.initiated_by current_user.id
+      @comp_requests = CompRequest.initiated_by current_user.id,comp_type
     end
 
   end
@@ -72,11 +75,10 @@ class CompRequestsController < ApplicationController
   # DELETE /connection_requests/1
   # DELETE /connection_requests/1.json
   def destroy
-
-    comp_requests = CompRequest.where(:id => params[:ids])
+    comp_requests = CompRequest.where(:id =>  params[:ids])
     comp_requests.each do |comp_request|
       comp_request.ignore
-      comp_request.destroy
+      ##comp_request.destroy
     end
     render json: {:status => :success}
 
