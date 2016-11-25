95c6eebcf9e9c95cd7210c092aa6228e1964094a
jahanzaib-dx <muhammad.jahanzaib@discretelogix.com>
2016-11-25 13:15:35 +0500

    image path resolve

--- app/models/back_end_custom_record.rb
+++ app/models/back_end_custom_record.rb
@@ -14,7 +14,7 @@ class BackEndCustomRecord < ActiveRecord::Base
     File.open(image_root + "#{name_folder}/" + new_file_name_with_type, "wb")  do |f|
       f.write(file)
     end
-    return "#{image_root}#{name_folder}/#{new_file_name_with_type}"
+    return "/public/uploads/custom_record/#{name_folder}/#{new_file_name_with_type}"
 
   end
 end
