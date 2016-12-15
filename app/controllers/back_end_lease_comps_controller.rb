250482e3811ee9fae8abbd59bccb71257e45e51f
jahanzaib-dx <muhammad.jahanzaib@discretelogix.com>
2016-12-15 20:01:14 +0500

    database duplication management

--- app/controllers/back_end_lease_comps_controller.rb
+++ app/controllers/back_end_lease_comps_controller.rb
@@ -40,9 +40,6 @@ class BackEndLeaseCompsController < ApplicationController
         counter+=1
       end
       ws.save()
-      # path = "#{Rails.root}/public/back_end_lease_comp/"
-      # extension = "4"
-      # session.drive.export_file(@file.id,extension,download_dest: "#{path}/#{@file.id}.xlsx")
 
       # save file name to database
       @BackEndLeaseComp = BackEndLeaseComp.new
@@ -155,6 +152,100 @@ class BackEndLeaseCompsController < ApplicationController
       counter+=1
     end
     deleted = ids.any? ? TenantRecord.where('id NOT IN (?) and user_id = ?',ids,@current_user) : TenantRecord.where('user_id = ?',@current_user)
+    deleted.destroy_all
+    redirect_to root_url
+  end
+
+  def duplication
+   tenant_records = TenantRecord.duplicate_list(current_user.id)
+   time = Time.now.getutc
+   fileName = Digest::SHA1.hexdigest("#{time}#{@current_user}")
+   session = GoogleDrive::Session.from_config("#{Rails.root}/config/google-sheets.json")
+
+     @file = session.drive.copy_file('12SnCrR6p2iMdGqKi05mL7TVj_-4CtV08rigwjA4rcP8', {name: fileName}, {})
+
+     # put data to sheet
+     ws = session.spreadsheet_by_key(@file.id).worksheets[0]
+     counter=2
+     tenant_records.each do |tenant_record|
+       ws[counter, 1] = tenant_record.id
+       ws[counter, 2] = 'Keep'
+       # ws[counter, 3] = tenant_record.image
+       ws[counter, 4] = tenant_record.comp_type
+       ws[counter, 5] = tenant_record.company
+       ws[counter, 6] = tenant_record.industry_type
+       ws[counter, 7] = tenant_record.address1
+       ws[counter, 8] = tenant_record.suite
+       ws[counter, 9] = tenant_record.city
+       ws[counter, 10] = tenant_record.state
+       ws[counter, 11] = tenant_record.submarket
+       ws[counter, 12] = tenant_record.class_type
+       ws[counter, 13] = tenant_record.property_type
+       ws[counter, 14] = tenant_record.property_name
+       ws[counter, 15] = tenant_record.lease_commencement_date
+       ws[counter, 16] = tenant_record.lease_term_months
+       ws[counter, 17] = tenant_record.free_rent
+       ws[counter, 18] = tenant_record.size
+       ws[counter, 19] = tenant_record.deal_type
+       ws[counter, 20] = 'Lease Structure'
+       ws[counter, 21] = tenant_record.base_rent
+       counter+=1
+     end
+     ws.save()
+
+     @file_temp = session.drive.copy_file(@file.id, {name: "#{@file.id}_temp"}, {})
+
+     session.drive.batch do
+       user_permission = {
+           value: 'default',
+           type: 'anyone',
+           role: 'writer'
+       }
+       session.drive.create_permission(@file_temp.id, user_permission, fields: 'id')
+     end
+     render :json => {
+         :file_temp => @file_temp.id
+     }
+
+  end
+
+  def delete_duplication
+    session = GoogleDrive::Session.from_config("#{Rails.root}/config/google-sheets.json")
+    ws = session.spreadsheet_by_key(params[:id]).worksheets[0]
+    tenant_records = TenantRecord.where('user_id = ?', @current_user)
+    counter=2
+    ids= Array.new
+    tenant_records.each do |tenant_record|
+      if TenantRecord.where(:id => ws[counter, 1]).present?
+        @tenant_record = TenantRecord.find_by(:id => ws[counter, 1])
+        @tenant_record.update_attributes(
+            # :image => ws[counter, 3],
+            :comp_type => ws[counter, 4],
+            :company => ws[counter, 5],
+            :industry_type => ws[counter, 6],
+            :address1 => ws[counter, 7],
+            :suite => ws[counter, 8],
+            :city => ws[counter, 9],
+            :state => ws[counter, 10],
+            :submarket => ws[counter, 11],
+            :class_type => ws[counter, 12],
+            :property_type => ws[counter, 13],
+            :property_name => ws[counter, 14],
+            :lease_commencement_date => ws[counter, 15],
+            :lease_term_months => ws[counter, 16],
+            :free_rent => ws[counter, 17],
+            :size => ws[counter, 18],
+            :deal_type => ws[counter, 19],
+            :lease_structure => ws[counter, 20],
+            :base_rent => ws[counter, 21]
+        )
+      end
+      if ws[counter,2] == 'Delete'
+        ids.push(ws[counter, 1])
+      end
+      counter+=1
+    end
+    deleted = TenantRecord.where('id IN (?) and user_id = ?',ids,@current_user)
     deleted.destroy_all
     redirect_to root_url
   end
--- app/controllers/back_end_sale_comps_controller.rb
+++ app/controllers/back_end_sale_comps_controller.rb
@@ -151,4 +151,93 @@ class BackEndSaleCompsController < ApplicationController
     deleted.destroy_all
     redirect_to root_url
   end
+
+  def duplication
+    sale_records = SaleRecord.duplicate_list(current_user.id)
+
+    time = Time.now.getutc
+    fileName = Digest::SHA1.hexdigest("#{time}#{@current_user}")
+    session = GoogleDrive::Session.from_config("#{Rails.root}/config/google-sheets.json")
+
+    @file = session.drive.copy_file('1CcONE2zcYygchHXZQvaw5FnwShJT76T6xRL8OX1sbUI', {name: fileName}, {})
+
+    # put data to sheet
+    ws = session.spreadsheet_by_key(@file.id).worksheets[0]
+    counter=2
+    sale_records.each do |sale_record|
+      ws[counter, 1] = sale_record.id
+      ws[counter, 2] = 'Keep'
+      # ws[counter, 3] = sale_record.image
+      # ws[counter, 4] = sale_record.comp_type
+      ws[counter, 5] = sale_record.address1
+      ws[counter, 6] = sale_record.city
+      ws[counter, 7] = sale_record.state
+      ws[counter, 8] = sale_record.submarket
+      ws[counter, 9] = sale_record.property_name
+      ws[counter, 10] = sale_record.build_date
+      ws[counter, 11] = sale_record.property_type
+      ws[counter, 12] = sale_record.class_type
+      ws[counter, 13] = sale_record.land_size
+      ws[counter, 14] = sale_record.price
+      ws[counter, 15] = sale_record.sold_date
+      ws[counter, 16] = sale_record.cap_rate
+      counter+=1
+    end
+    ws.save()
+
+    @file_temp = session.drive.copy_file(@file.id, {name: "#{@file.id}_temp"}, {})
+
+    session.drive.batch do
+      user_permission = {
+          value: 'default',
+          type: 'anyone',
+          role: 'writer'
+      }
+      session.drive.create_permission(@file_temp.id, user_permission, fields: 'id')
+    end
+    render :json => {
+        :file_temp => @file_temp.id
+    }
+
+  end
+
+  def delete_duplication
+    session = GoogleDrive::Session.from_config("#{Rails.root}/config/google-sheets.json")
+    ws = session.spreadsheet_by_key(params[:id]).worksheets[0]
+    sale_records = SaleRecord.where('user_id = ?', @current_user)
+    counter=2
+    ids= Array.new
+    sale_records.each do |sale_record|
+      # if sale_record.id!=counter-1
+      #   ws[counter, 1] =  counter
+      #   next
+      # end
+      if SaleRecord.where(:id => ws[counter, 1]).present?
+        @sale_record = SaleRecord.find_by(:id => ws[counter, 1])
+        @sale_record.update_attributes(
+            # :image => ws[counter, 3],
+            # :comp_type => ws[counter, 4],
+            :address1 => ws[counter, 5],
+            :city =>  ws[counter, 6],
+            :state => ws[counter, 7],
+            :submarket => ws[counter, 8],
+            :property_name => ws[counter, 9],
+            :build_date => ws[counter, 10],
+            :property_type => ws[counter, 11],
+            :class_type => ws[counter, 12],
+            :land_size => ws[counter, 13],
+            :price => ws[counter, 14],
+            :sold_date => ws[counter, 15],
+            :cap_rate => ws[counter, 16]
+        )
+      end
+      if ws[counter,2] == 'Delete'
+        ids.push(ws[counter, 1])
+      end
+      counter+=1
+    end
+    deleted = SaleRecord.where('id IN (?) and user_id = ?',ids,@current_user)
+    deleted.destroy_all
+    redirect_to root_url
+  end
 end
