69a01b946f96be00489ba184cbdf020bb4e2df87
aamir-sarwar <aamir.sarwar@discretelogix.com>
2016-12-19 16:19:35 +0500

    Full Transparency

--- app/assets/javascripts/comp_requests.js
+++ app/assets/javascripts/comp_requests.js
@@ -71,6 +71,91 @@ jQuery("document").ready(function (){
 
         return false;
     });
+    
+    //-----------------------------------------------------------------------------------------------------------
+    
+    // full transprancy
+    jQuery("#btn-grant-access").on("click", function (){
+    	$('#Modalfull').modal('hide');
+        displayOverlay = true;
+        //$('#basicModal').modal('hide');
+        var $btn = jQuery(this);
+        
+        var $selected_check_obj = $(".checkbox-info input:checkbox:checked")
+        
+        if ($selected_check_obj.size() > 1) {
+        	// error 
+        	uiAlert('Error!','Select only one comp to grant access');
+        	return;
+        }
+        
+        if ($selected_check_obj.size() < 1) {
+        	// error 
+        	uiAlert('Error!','Select one comp to grant access');
+        	return;
+        }
+        
+        var selected_comp_id = $selected_check_obj.val();
+        $("#access_comp_id").val(selected_comp_id);
+        
+        
+        var $form = $btn.parents('form')
+        var dataString = $form.serialize();
+        //console.log($form.attr('action'));
+        //console.log(dataString);
+        $.ajax({
+            type: "POST",
+            url: $form.attr('action'),
+            data: dataString,
+            dataType: "json",
+            success: function(data) {
+                //console.log(data);
+                if(data.status == 'success'){
+                	$selected_check_obj.parents('tr').slideUp();
+                    uiAlert('Success','Access for selected comps granted successfully');
+                    
+                }else{
+                    uiAlert('Error!','Unable to grant access. '+data.message);
+                    // if(data.issue == 'Mobile Validation'){
+                        // document.location.href = data.url;
+                    // }else {
+                        // $('#basicModal').modal('show');
+                    // }
+                }
+            }
+        });
+
+        return false;
+    });
+    
+    $(function(){
+	    $("input:checkbox.chkfull").click(function(){
+	      $("input:checkbox.chkfull").not($(this)).removeAttr("checked");
+	      $(this).attr("checked", $(this).attr("checked"));    
+	    });
+	});
+	
+	$(function(){
+	    $("#checkbox1,.checkbox-info input:checkbox").click(function(){
+	    	var checked_check = $(".checkbox-info input:checkbox:checked").size();
+	    	
+	    	if (checked_check > 1) {
+	    		$(".active_trans").hide();
+	    		$(".inactive_trans").show();
+	    		$(".active_trans").parents('li').removeClass("active-locl");
+	    	} else {
+	    		$(".active_trans").show();
+	    		$(".inactive_trans").hide();
+	    	}
+	    	
+	    });
+	});
+	
+	$(function(){
+	    $(".tx_lock_list li").click(function(e){	    	
+	      $(".inactive_trans").parents('li.active-locl').removeClass("active-locl");  
+	    });
+	});
 
 
 });
