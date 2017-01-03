0b1cbf6867c20b3ebbd8690b84136b566a96bc9f
aamir-sarwar <aamir.sarwar@discretelogix.com>
2017-01-03 15:09:54 +0500

    Emails updated in activity log

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -14,9 +14,27 @@ class ActivityLogsController < ApplicationController
     
     ##@activity_logs = []
     @activity_logs = activity_logs.each do |activity|
-      unlockFields = SharedComp.getUnlockData activity
       
-      lockData activity, unlockFields 
+      if activity.comptype == 'lease'
+        activity_comp = activity.tenant_record
+        activity_comp.base_rent = number_to_currency(activity_comp.base_rent.to_f, {:precision=>2})
+        activity_comp.net_effective_per_sf = number_with_precision(activity_comp.net_effective_per_sf,:precision => 2)
+      else
+        activity_comp = activity.sale_record
+        activity_comp.price = number_to_currency(activity_comp.price.to_f, {:precision=>2})
+      end
+      
+      ##if activity.reciver_id == current_user.id
+        ##lockData activity, []
+      
+      if activity.status == 'Rejected'
+        lockData activity, []
+            
+      elsif activity.status == 'partial'
+        unlockFields = SharedComp.getUnlockData activity
+      
+        lockData activity, unlockFields 
+      end
       
       
       
@@ -46,19 +64,23 @@ class ActivityLogsController < ApplicationController
       activity_comp = activity.tenant_record
       
       activity_comp.company = if unlockFields.include?"company" then activity_comp.company else 'Lock' end
-      activity_comp.base_rent = if unlockFields.include?"base_rent" then number_to_currency(activity_comp.base_rent.to_f, {:precision=>2}) else 'Lock' end
-      activity_comp.net_effective_per_sf = if unlockFields.include?"net_effective_per_sf" then number_with_precision(activity_comp.net_effective_per_sf,:precision => 2) else "-0.0" end
+      activity_comp.base_rent = if unlockFields.include?"base_rent" then activity_comp.base_rent else 'Lock' end
+      activity_comp.net_effective_per_sf = if unlockFields.include?"net_effective_per_sf" then activity_comp.net_effective_per_sf else "-0.0" end
 
     else
       activity_comp = activity.sale_record
       
-      activity_comp.price = if unlockFields.include?"price" then number_to_currency(activity_comp.price.to_f, {:precision=>2}) else '-0.0' end
+      activity_comp.price = if unlockFields.include?"price" then activity_comp.price else '-0.0' end
       activity_comp.land_size = if unlockFields.include?"land_size" then activity_comp.land_size else '-0.0' end
       ##activity_comp.net_effective_per_sf = if unlockFields.include?"net_effective_per_sf" then number_with_precision(activity_comp.net_effective_per_sf,:precision => 2) else "-0" end
 
     end
 
       activity
+  end
+  
+  def lockDefault
+    
   end
   
 end
