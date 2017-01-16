8070bc90b1ea53c5db6476e114474a6fed8b48e8
aamir-sarwar <aamir.sarwar@discretelogix.com>
2017-01-16 16:56:09 +0500

    SF range updated on Activity Pages Image updated on Activity Pages

--- app/models/tenant_record.rb
+++ app/models/tenant_record.rb
@@ -1,7 +1,18 @@
 class TenantRecord < ActiveRecord::Base
 
   include ConditionalValidations
+  
+  ##:sizerange = ''
+  ##cattr_reader :size_range123, :instance_reader => false
+  ##cattr_reader :sizerange
+  mattr_accessor :sizerange
+  ##self.sizerange="range"
+  ##cattr_reader :code, :description, :points, :instance_reader => false
 
+ ##@@size_range123 = "EXM"
+ 
+   ##self.test = 123
+  
   #acts_as_paranoid
   store_accessor :data
 
