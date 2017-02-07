f9fc37a49f7ea19886233eb043890e9692ad431b
jahanzaib-dx <muhammad.jahanzaib@discretelogix.com>
2017-02-07 12:43:04 +0500

    1) Test 1 Fail. Dup was added. Then I went to delete the dup and received the following screen shot message. 2) Test 2 Fail. Changed "industry" and dup did not show. It should. See image Done.

--- app/controllers/back_end_lease_comps_controller.rb
+++ app/controllers/back_end_lease_comps_controller.rb
@@ -433,36 +433,36 @@ class BackEndLeaseCompsController < ApplicationController
         counter+=1
       end
     else
-
+      error_string='<br>'
       tenant_records.each do |tenant_record|
         # while ws[counter,1] != tenant_record.id.to_s
         #   counter+=1
         # end
-        if TenantRecord.where(:id => ws[counter, 1]).present?
+        if TenantRecord.where(:id => ws[counter, 1]).present? and ws[counter,2] == 'Keep'
           @tenant_record = TenantRecord.find_by(:id => ws[counter, 1])
-          error_string += (ws[counter, 4] == '')? "</br>Cell no. C#{counter} is required" : ""
-          error_string += (ws[counter, 5] == '')? "</br>Cell no. D#{counter} is required" : ""
-          error_string += (ws[counter, 6] == '')? "</br>Cell no. E#{counter} is required" : ""
+          error_string += (ws[counter, 4] == '')? "</br>Cell no. D#{counter} is required" : ""
+          error_string += (ws[counter, 5] == '')? "</br>Cell no. E#{counter} is required" : ""
+          error_string += (ws[counter, 6] == '')? "</br>Cell no. F#{counter} is required" : ""
           @tenant_record.address1 = ws[counter, 7]
           @tenant_record.city = ws[counter, 9]
           @tenant_record.state = ws[counter, 10]
           result = validate_address_google(@tenant_record,true)
           if result.has_key? :errors
-            error_string += (result[:errors][:geocode_info].to_s != '') ? "</br>Cell no. F#{counter} "+result[:errors][:geocode_info].to_s : ""
+            error_string += (result[:errors][:geocode_info].to_s != '') ? "</br>Cell no. G#{counter} "+result[:errors][:geocode_info].to_s : ""
           end
-          error_string += (ws[counter, 7] == '')? "</br>Cell no. F#{counter} is required" : ""
-          error_string += (ws[counter, 8] == '')? "</br>Cell no. G#{counter} is required" : ""
+          error_string += (ws[counter, 7] == '')? "</br>Cell no. G#{counter} is required" : ""
+          error_string += (ws[counter, 8] == '')? "</br>Cell no. H#{counter} is required" : ""
           # error_string += (ws[counter, 9] == '')? "</br>Cell no. H#{counter} is required" : ""
-          error_string += (ws[counter, 10] == '')? "</br>Cell no. I#{counter} is required" : ""
-          error_string += (ws[counter, 11] == '')? "</br>Cell no. J#{counter} is required" : ""
-          error_string += (ws[counter, 12] == '')? "</br>Cell no. K#{counter} is required" : ""
-          error_string += (ws[counter, 13] == '')? "</br>Cell no. L#{counter} is required" : ""
-          error_string += (ws[counter, 15] == '')? "</br>Cell no. N#{counter} is required" : ""
-          error_string += (ws[counter, 16] == '')? "</br>Cell no. O#{counter} is required" : ""
-          error_string += (ws[counter, 18] == '')? "</br>Cell no. Q#{counter} is required" : ""
-          error_string += (ws[counter, 19] == '')? "</br>Cell no. R#{counter} is required" : ""
-          error_string += (ws[counter, 20] == '')? "</br>Cell no. S#{counter} is required" : ""
-          error_string += (ws[counter, 21] == '')? "</br>Cell no. T#{counter} is required" : ""
+          error_string += (ws[counter, 10] == '')? "</br>Cell no. J#{counter} is required" : ""
+          error_string += (ws[counter, 11] == '')? "</br>Cell no. K#{counter} is required" : ""
+          error_string += (ws[counter, 12] == '')? "</br>Cell no. L#{counter} is required" : ""
+          error_string += (ws[counter, 13] == '')? "</br>Cell no. M#{counter} is required" : ""
+          error_string += (ws[counter, 15] == '')? "</br>Cell no. O#{counter} is required" : ""
+          error_string += (ws[counter, 16] == '')? "</br>Cell no. P#{counter} is required" : ""
+          error_string += (ws[counter, 18] == '')? "</br>Cell no. R#{counter} is required" : ""
+          error_string += (ws[counter, 19] == '')? "</br>Cell no. S#{counter} is required" : ""
+          error_string += (ws[counter, 20] == '')? "</br>Cell no. T#{counter} is required" : ""
+          error_string += (ws[counter, 21] == '')? "</br>Cell no. U#{counter} is required" : ""
           error_string += (
               (ws[counter, 25] == ''  || ws[counter, 25] == "0") &&
               (ws[counter, 26] == '' || ws[counter, 26] == "0") &&
@@ -473,7 +473,7 @@ class BackEndLeaseCompsController < ApplicationController
       end
     end
     delete_url = (params[:id].present?)? "/back_end_lease_comps/create/#{params[:id]}/#{params[:temp]}" : "/back_end_lease_comps/delete_duplication/#{params[:temp]}";
-    if error_string==''
+    if error_string=='' || error_string=='<br>'
       render json:{
           flag: 'ok',
           url: delete_url
